0d0f5fbfd879c86ef78382ecacc48267bb23c4b5
Elliot <05elliotp@gmail.com>
2025-03-07 10:45:01 +0000

    fixed bug mitch found

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -25,7 +25,7 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
             );
         })
         .then(() => {
-            const converted = topicData.map((obj) => [obj.description, obj.slug, obj.img_url])
+            const converted = topicData.map((obj) => [obj.slug, obj.description, obj.img_url])
             return db.query(format("INSERT INTO topics(slug, description, img_url) VALUES %L", converted))
         })
         .then(() => {
@@ -39,7 +39,7 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
 
             const converted = fixedTime.map((obj) => [obj.title, obj.topic, obj.author, obj.body, obj.created_at, obj.votes, obj.article_img_url])
 
-            return db.query(format("INSERT INTO articles(title, topic, author, body, created_at, votes, article_img_url) VALUES %L", converted))
+            //return db.query(format("INSERT INTO articles(title, topic, author, body, created_at, votes, article_img_url) VALUES %L", converted))
         })
         .then(() => {});
 };
