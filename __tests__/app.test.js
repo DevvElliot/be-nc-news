194956776ab5cc2797f8fd318bf1f75a66aae2e3
Elliot <05elliotp@gmail.com>
2025-03-10 16:55:11 +0000

    setup topics api, will handle errors tomorrow

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,15 @@
 const endpointsJson = require("../endpoints.json");
-const db = require("../db/connection")
-const seed = require("../db/seeds/seed")
-const data = require("../db/data/test-data/index")
-const request = require("supertest")
-const app = require("../app")
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data/index");
+const request = require("supertest");
+const app = require("../app");
 /* Set up your test imports here */
 
 /* Set up your beforeEach & afterAll functions here */
-beforeEach(() => seed(data))
+beforeEach(() => seed(data));
 
-afterAll(() => db.end())
+afterAll(() => db.end());
 
 describe("GET /api", () => {
     test("200: Responds with an object detailing the documentation for each endpoint", () => {
@@ -18,6 +18,21 @@ describe("GET /api", () => {
             .expect(200)
             .then(({ body: { endpoints } }) => {
                 expect(endpoints).toEqual(endpointsJson);
+            });
+    });
+});
+
+describe("GET /api/topics", () => {
+    test("200: responds with an array of topics", () => {
+        return request(app)
+            .get("/api/topics")
+            .expect(200)
+            .then(({ body }) => {
+                expect(body.topics.length).toBe(3);
+                body.topics.forEach((topic) => {
+                    expect(typeof topic.slug).toBe("string");
+                    expect(typeof topic.description).toBe("string");
+                });
             });
     });
 });
